68681b54ccc4b49ecaaa463da21366c1e078d1ab
tsuyoshi-alh61 <sato.tsuyoshi@alhinc.jp>
2021-12-04 17:30:23 +0900

    installed Laravel

---
+++ todo-list/resources/js/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/app.css';
+import Example from './components/Example';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Example />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
